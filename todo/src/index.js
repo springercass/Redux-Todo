adf474c9b15aae2339179273c2d071a50cb5e48c
springercass <casprin2@gmail.com>
2019-06-18 16:53:22 -0500

    redux.js.org example todo list imported

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import { render } from "react-dom";
 import { Provider } from "react-redux";
-import * as serviceWorker from "./serviceWorker";
+import { createStore } from "redux";
+import rootReducer from "./reducers";
+import App from "./App";
 
-const store = createStore(todo);
+const store = createStore(rootReducer);
 
-ReactDOM.render(
-  <Provider store={createStore(todo)}>
+render(
+  <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById("root")
@@ -17,4 +17,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//serviceWorker.unregister();
